86498d7e92e9f3b6963df91dbb1906f56c20649b
Hactys <victor.cachau@free.fr>
2025-12-31 15:30:40 +0100

    Fix : remove attacker from board on capture failure.

--- game_engine/actions/move.py
+++ game_engine/actions/move.py
@@ -115,7 +115,7 @@ class MoveAction(BaseAction):
                 board.remove_piece(self.to_pos)
                 board.set_piece(self.to_pos, attacker)
             else:  # Attaquant détruit
-                board.remove_piece(self.to_pos)
+                board.remove_piece(self.from_pos)
 
         # Passage au tour de l'adversaire
         game.end_turn()
